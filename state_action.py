774bf38fb7de6ed29dca04b4f37c351dd0dcd931
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2022-07-06 17:16:44 -0400

    working on TODO

--- state_action.py
+++ state_action.py
@@ -3,7 +3,19 @@ from abc import ABC,abstractmethod
 import numpy as np
 
 class State():
-    def __init__(self, _state, _V = 0, _policy = None, _is_terminal = False):
+    """
+    Description: A class for representing states in MDPs. This includes sub states, possible actions, the value, policy, and whether or not a state is terminal, and hash function
+    User defines:
+        _state (dict): dictionary consisting of substates
+        _action (list(Action)): list of actions to take
+        _V (float): Initial estimate of the value
+        _is_terminal (bool): is true if state is terminal
+        _policy (int): Action to select
+    """
+    def __init__(self, _state, _action = None, _V = 0, _is_terminal = False, _policy = None):
+        """
+        Constructor
+        """
         super(State, self).__init__()
 
         self.V_ = _V
@@ -11,46 +23,97 @@ class State():
         self.is_terminal_ = _is_terminal
 
         self.s_ = _state
-        
-        #how to structure children? Need to have actions, then actions carry
 
         if type(_state) is dict:
             self.s_ = _state
         else:
             self.s_ = {'x': _state}
+            
+        if type(_action) is list:
+            self.a_ = _action
+        else:
+            self.a_ = []
 
     def __copy__(self):
+        """
+        copy function
+
+        Returns:
+            State: copied state
+        """
         return State(self.s_, self.V_, self.policy_, self.is_terminal_)
     
     def __hash__(self):
+        """
+        Maps state values to hash key
+
+        Returns:
+            string: hash key
+        """
         pass
     
-    def add_child(self, _a, _s):
+    def add_child(self, _a):
+        """
+        Adds child action to state
+
+        Args:
+            _a (Action): 
+        """
+        
+        pass
+    
+    def get_transition(self, _a):
+        """
+        Gets transition model associated with state
+
+        Returns:
+            list(tuple): transition model associated with state
+        """
+        
         pass
 
     @abstractmethod
     def __eq__(self, _s):
+        """
+        Constructor, initializes MDP model
+
+        Args:
+            self (VI_Adversary): Object to initialize
+            _epsilon (double): convergence criteria
+            _gamma (double): Discount factor
+            _num_actions (int): number of actions
+
+        Returns:
+            VI_Adversary: Q-value iteration object
+        """
         return (self.s_ == _s.s_)
     
 class Action():
-    def __init__(self, _action):
+    def __init__(self, _action, _gamma):
         super(Action, self).__init__()
 
         self.a_ = _action
+        self.gamma_ = _gamma
         self.s_prime_ = []
         self.r_ = []
         self.n_ = []
+        self.N_ = 0
     
     def add_child(self, _s):
         # if state already added
             #increment n and add count to r
         # else 
             # add s, r, n to list
-        pass
+        self.N_ += 1
     
     def get_distribution(self):
         dist = []
+        # get values, compute r + gamma*V
+        t = self.n_ / self.N_
+        #generate pairs and add to dist
         return dist
+        #assume upper and lower bound/DST will be done by optimizer
+
 
     @abstractmethod
     def __eq__(self, _s):
