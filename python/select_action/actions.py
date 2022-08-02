b1e1d11eded9d65c37167436a2c95afc9d4aba3f
rmb0034 <rmb0034@mix.wvu.edu>
2022-08-02 12:41:48 -0400

    Changed actions and MCGS files

--- python/select_action/actions.py
+++ python/select_action/actions.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-
+import random
 
 class action_selection():
     """
@@ -22,21 +22,21 @@ class action_selection():
         pass
 
 
-    def return_action(self,_t,_s,_a,_param):
-        return self.func_(_t,_s,_a,_param, self.const_)
+    def return_action(self,_s,_a,_param = []):
+        return self.func_(_s,_a, self.const_,_param)
 
 
-
-def UCB1(_t,_s,_a,_param,_const):
+def UCB1(_s,_a,_const,_param=[]):
     UCB = math.nan
     optAction = math.nan
+    random.shuffle(_a)
     for a in _a:
-        aVal =_param[a]["Q"] + _const["c"]*np.sqrt(((2*np.log(_t))/_param[a]["N"])) #UCB1 Equation
+        aVal =_param[a]["Q"] + _const["c"]*np.sqrt(((2*np.log(_s.t_))/_param[a]["N"])) #UCB1 Equation
         if aVal > UCB or np.isnan(UCB):
             UCB = aVal
             optAction = a
     return optAction
 
 
-def randomAction(_t,_s,_a,_param,_const):
+def randomAction(_s,_a,_const,_param):
     return np.random.randint(len(_a))
