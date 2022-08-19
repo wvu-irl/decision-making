7b73575ebd6eebdc8e906e6e26e386618ff695f5
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2022-08-18 20:02:55 -0400

    working gridworld

--- python/gym_envs/gridworld.py
+++ python/gym_envs/gridworld.py
@@ -41,7 +41,7 @@ class GridWorld(gym.Env):
         - black: puddle
     """
 
-    def __init__(self, _p = 0, _goal=None):
+    def __init__(self, _dim = [40, 40], _goal= [10, 10], _p = 0):
         """
         Constructor, initializes state
         Args:
@@ -52,33 +52,14 @@ class GridWorld(gym.Env):
         """
         super(GridWorld, self).__init__()
 
-        self.seed_length_ = 4
-
-        self.map_ = _map
-        self.map_size_ = np.shape(_map)
-        self.puddle_t_ = _T
-        self.B_, self.B_unambig_ = self.compute_B(_T)
-        self.T_p_ = self.compute_pignistic(self.B_)
-        # for x in range(self.map_size_[0]):
-        #     for y in range(self.map_size_[1]):
-        #         for a in range(9):
-        #             print(x, " | ", y, " | ", a, " | ", self.T_p_[x][y][a])
-        #         print("----------")
-        self.T_unambig_p_ = self.compute_pignistic(self.B_unambig_)
-        #print(self.T_unambig_p_[0][0])
-        self.sample_T()
+        self.map_ = np.zeros(_dim)
+        self.dim_ = _dim
         
-        # for x in range(self.map_size_[0]):
-        #     for y in range(self.map_size_[1]):
-        #         for a in range(9):
-        #             print(x, " | ", y, " | ", a, " | ", self.T_[x][y][a])
-        #         print("----------")
-        # print(self.T_[0][0])
-        # print(self.T_p_[5][5])
-        # print(self.T_unambig_p_[5][5])
-        self.R_ = _R
-        self.reinit(_map, _seed)
-        self.img_num_ = 0
+        
+        self.p_ = _p
+        self.reinit(_goal)
+        
+        self.a_ = [0, 1, 2, 3]
         
         self.fig_ = plt.figure()
         self.ax_ = self.fig_.add_subplot(1,1,1)
@@ -86,57 +67,29 @@ class GridWorld(gym.Env):
         self.rng_ = np.random.default_rng()
         
     def get_num_states(self):
-        return self.map_size_
+        return self.dim_[0]*self.dim_[1]
     
     def get_num_actions(self):
-        return 9
+        return 4
     
-    def reinit(self, _map = None, _seed = None):
-        if _seed != None:
-            self.seed_ = _seed  
-        else:
-            self.seed_ = self.random_seed()
+    def reinit(self, _goal = None):
+        self.agent_ = [np.floor(self.dim_[0]/2), np.floor(self.dim_[1]/2)] 
+        if _goal != None:
+            self.goal_ = _goal
         
-        self.agent_, self.goal_ = self.seed_2_env(self.seed_)
-        self.agent_prev_ = self.agent_.copy()
-        self.action_ = 9    
-        
-    def random_seed(self):
-        seed = np.empty([self.seed_length_, 1], dtype=int)
-        for i in range(self.seed_length_):
-            seed[i] = random.randint(0, 9999)
-        return seed
-
-    def env_2_seed(self, _agent, _goal):
-        seed = [0]* self.seed_length_
-
-        seed[0] = np.round(_agent[0]/(self.map_size_[0]-1) * 9999)
-        seed[1] = np.round(_agent[1]/(self.map_size_[1]-1) * 9999)
-        seed[2] = np.round(_goal[0]/(self.map_size_[0]-1) * 9999)
-        seed[3] = np.round(_goal[1]/(self.map_size_[1]-1) * 9999)
-
-        return seed
-
-    def seed_2_env(self, _seed):
-        agent = [0,0]
-        goal = [0,0]
-        
-        agent[0] = np.round(_seed[0]*(self.map_size_[0]-1)/9999)
-        agent[1] = np.round(_seed[1]*(self.map_size_[1]-1)/9999)
-        goal[0] = np.round(_seed[2]*(self.map_size_[0]-1)/9999)
-        goal[1] = np.round(_seed[3]*(self.map_size_[1]-1)/9999)
-        
-        return agent, goal  
+        for i in range(self.dim_[0]):
+            for j in range(self.dim_[1]):
+                self.map_[i][j] = self.get_reward([i,j])
     
     def render(self, fp = None):
             #plt.clf()
         
         plt.cla()
         plt.grid()
-        size = 100/self.map_size_[0]
+        size = 100/self.dim_[0]
         # Render the environment to the screen
         t_map = (self.map_)
-        plt.imshow(np.transpose(t_map), cmap='binary', interpolation='hanning')
+        plt.imshow(np.transpose(t_map), cmap='Reds', interpolation='hanning')
         if self.agent_[0] != self.goal_[0] or self.agent_[1] != self.goal_[1]:
             plt.plot(self.agent_[0], self.agent_[1],
                      'bo', markersize=size)  # blue agent
@@ -146,47 +99,56 @@ class GridWorld(gym.Env):
             plt.plot(self.goal_[0], self.goal_[1],
                      'bX', markersize=size) # agent and goal
 
-        ticks = np.arange(-0.5, self.map_size_[0]-0.5, 1)
+        ticks = np.arange(-0.5, self.dim_[0]-0.5, 1)
         self.ax_.set_xticks(ticks)
         self.ax_.set_yticks(ticks)
         plt.xticks(color='w')
         plt.yticks(color='w')
         plt.show(block=False)
-        if fp != None:
-            self.fig_.savefig(fp +"%d.png" % self.img_num_)
-            self.img_num_ += 1
+        # if fp != None:
+        #     self.fig_.savefig(fp +"%d.png" % self.img_num_)
+        #     self.img_num_ += 1
         plt.pause(1)
         #plt.close() 
         
     def get_observation(self):
-        return [int(self.agent_[0]), int(self.agent_[1])], [int(self.goal_[0]), int(self.goal_[1])]
+        return [int(self.agent_[0]), int(self.agent_[1])]
     
-    def get_reward(self, s1=None, a=None, s2=None):
-        if s1 == None:
-            s1 = self.agent_
-        if s2 == None:
-            s2 = self.agent_prev_
-            
-        if get_distance(s1,self.goal_):
-            return -self.R_[0]*get_distance(s1,s2) - self.R_[1]
+    
+    def get_distance(self, s1, s2):
+        return np.sqrt( (s1[0]-s2[0])**2 + (s1[1]-s2[1])**2 )
+    
+    
+    def get_reward(self, _s):
+        d = self.get_distance(_s, self.goal_)
+        if d >= 5:
+            return 0
         else:
-            return self.R_[2]
+            return 1 - (d**2)/25
     
-    def sample_distribution(self, _distribution):
-        t_sum = 0
+    def sample_transition(self, _action):
         p = self.rng_.uniform()
-        # print(p)
-        # print(_distribution)
-        for i in range(len(_distribution)):
-            t_sum += _distribution[i]
-            if t_sum >= p:
-                # print("i", i)
-                return i
+
+        if p < self.p_:
+            t = self.a_.copy()
+            t.remove(_action)
+            _action = self.rng_.choice(t)     
+        return _action
     
     def step(self, _action):
-        self.agent_prev_ =self.agent_.copy()
-        state_ind = self.sample_distribution(self.T_[int(self.agent_[0])][int(self.agent_[1])][_action])
-        self.agent_ = self.get_coordinate_move(self.agent_, state_ind)
+        print("------")
+        print(_action)
+        _action = self.sample_transition(_action)
+        print(_action)
+        self.agent_ = self.get_coordinate_move(self.agent_, _action)
+        
+        
+        r = self.get_reward(self.agent_)
+        if r == 1:
+            done = True
+        else:
+            done = False
+        return self.agent_, r, done, []
         
     def get_actions(self, _agent):
         n, a = self.get_neighbors(_agent)
@@ -201,31 +163,10 @@ class GridWorld(gym.Env):
             t[0] += step[i][0]
             t[1] += step[i][1]
             
-            if t[0] >= 0 and t[1] >= 0 and t[0] < self.map_size_[0] and t[1] < self.map_size_[1]:
+            if t[0] >= 0 and t[1] >= 0 and t[0] < self.dim_[0] and t[1] < self.dim_[1]:
                 neighbors.append(t)
                 neighbors_ind.append(i)
         return neighbors, neighbors_ind
-    
-    def get_transitions(self, _agent, _action):
-        n, n_ind = self.get_neighbors(_agent)
-        t = []
-        for idx in n_ind:
-            t.append(self.T_[_agent[0]][_agent[1]][_action][idx])
-        return n, t 
-    
-    def get_transition_model(self, _agent, _action, _ambiguity = True):
-        if _ambiguity:
-            T = self.T_p_
-        else:
-            T = self.T_unambig_p_
-        
-            
-        n, n_ind = self.get_neighbors(_agent)
-        t = []
-        for idx in n_ind:
-            # print(_agent[0], "|", _agent[1], "|", _action, "|", idx)
-            t.append(T[_agent[0]][_agent[1]][_action][idx])
-        return n, t 
 
     def get_coordinate_move(self, _position, _action):
         _position = _position.copy()
@@ -233,19 +174,11 @@ class GridWorld(gym.Env):
         if   _action == 0:
             step = [ 0, -1] # S
         elif _action == 1:
-            step = [-1, -1] # SW
-        elif _action == 2:
             step = [-1,  0] #  W
-        elif _action == 3:
-            step = [-1,  1] # NW
-        elif _action == 4:
+        elif _action == 2:
             step = [ 0,  1] # N
-        elif _action == 5:
+        elif _action == 3:
             step = [ 1,  1] # NE
-        elif _action == 6:
-            step = [ 1,  0] #  E
-        elif _action == 7:
-            step = [ 1, -1] # SE
         else:
             step = [0, 0]   #  Z
 
@@ -255,109 +188,22 @@ class GridWorld(gym.Env):
         
         if temp[0] < 0:
             temp[0] = 0
-        if temp[0] >= self.map_size_[0]:
-            temp[0] = self.map_size_[0]-1
+        if temp[0] >= self.dim_[0]:
+            temp[0] = self.dim_[0]-1
         if temp[1] < 0:
                 temp[1] = 0
-        if temp[1] >= self.map_size_[1]:
-            temp[1] = self.map_size_[1]-1
+        if temp[1] >= self.dim_[1]:
+            temp[1] = self.dim_[1]-1
         return temp
 
     def get_action(self, _action):
         if   _action[0] ==  0 and _action[1] == -1:
             return 0 # S
-        elif _action[0] == -1 and _action[1] == -1:
-            return 1 # SW
         elif _action[0] == -1 and _action[1] ==  0:
-            return 2 #  W
-        elif _action[0] == -1 and _action[1] ==  1:
-            return 3 # NW
+            return 1 #  W
         elif _action[0] ==  0 and _action[1] ==  1:
-            return 4 # N
-        elif _action[0] ==  1 and _action[1] ==  1:
-            return 5 # NE
+            return 2 # N
         elif _action[0] ==  1 and _action[1] ==  0:
-            return 6 #  E
-        elif _action[0] ==  1 and _action[1] == -1:
-            return 7 # SE
+            return 3 #  E
         else:
-            return 8 # Z
-        
-    def sample_T(self):
-        T = [None]*self.map_size_[0]
-        for x in range(self.map_size_[0]):
-            Ty = [None]*self.map_size_[1]
-            for y in range(self.map_size_[1]):
-                Ta = [None]*9
-                for a in range(9):
-                    if self.B_[x][y][a] != None:
-                        # print(x, " | ", y, " | ", a, " | ", self.T_p_[x][y][a])
-                        # print(self.B_[x][y][a].focal_elements) 
-                        Ta[a] = self.B_[x][y][a]._sample_nonzero_probability()
-                        # print(Ta[a])
-                Ty[y] = Ta
-            T[x] = Ty
-        self.T_ = T
-                
-    # for bf, dempster combo is done with a list of a set of elements and a list of their corresponding masses      
-    def compute_B(self, _T):
-        B = [None]*self.map_size_[0]
-        #print("a",np.shape(B))
-        Bu = [None]*self.map_size_[0]
-        sln_space = list(range(9))
-        for x in range(self.map_size_[0]):
-            By = [None]*self.map_size_[1]
-            Buy = [None]*self.map_size_[1]
-            for y in range(self.map_size_[1]):
-                Ba = [None]*9
-                Bau = [None]*9
-                actions = self.get_actions([x,y])
-                la = len(actions)
-                for a in range(9):
-                    bf = BeliefFunction(sln_space)
-                    bfu = BeliefFunction(sln_space)                    
-                    if a in actions:   
-                        if la < 9:
-                            bf._dempster_combination([actions],[1])
-                            bfu._dempster_combination([actions],[1])
-                        
-                        if self.map_[x][y] == 1:
-                            #fe = [[a], [8], actions]
-                            if a != 8:
-                                fe = [[a], [8], [a,8]]
-                                m = [_T[0]*(1-_T[1]),    (1-_T[0])*(1-_T[1]),     _T[1]]
-                            else:
-                                fe = [[8]]
-                                m = [1]
-                            
-                            bf._dempster_combination(fe,m)
-                            fe = [[a], [8]]
-                            m = [_T[0],    1-_T[0]]
-                            bfu._dempster_combination(fe,m)
-                            
-                        else:
-                            bf._dempster_combination([[a]],[1])
-                            bfu._dempster_combination([[a]],[1])
-                    else:
-                        bf._dempster_combination([[8]],[1])
-                        bfu._dempster_combination([[8]],[1])
-                    Ba[a] = bf
-                    Bau[a] = bfu 
-                By[y] = Ba
-                Buy[y] = Bau
-            B[x] = By
-            Bu[x] = Buy
-        return B, Bu
-    
-    def compute_pignistic(self, B):
-        T = [None]*self.map_size_[0]
-        for x in range(self.map_size_[0]):
-            Ty = [None]*self.map_size_[1]
-            for y in range(self.map_size_[1]):
-                Ta = [None]*9
-                actions = self.get_actions([x,y])
-                for a in range(9):#actions:              
-                    Ta[a] = B[x][y][a]._get_pignistic_prob()
-                Ty[y] = Ta
-            T[x] = Ty
-        return T
+            return 4 # Z
--- python/test/test_world_ex.py
+++ python/test/test_world_ex.py
@@ -7,38 +7,22 @@ sys.path.append(parent)
 import numpy as np
 import random
 
-from solver.optimization import Bellman, PignisticBellman
-from solver.VI import VI
+from gym_envs.gridworld import GridWorld
 
-from env.AmbiguousPuddleWorld import AmbiguousPuddleWorld
-from env.PuddleWorldGen import PuddleWorldGen
 
 ## Params
-world_size = 10
-puddle_transition = [0.9, 0.5]
-R = [0, 1, 50]
 
-map = PuddleWorldGen(world_size,world_size,0)
-map.add_rectangle_puddle(3, 3, 2, 2)
-map.add_rectangle_puddle(4, 4, 2, 2)
-map.add_rectangle_puddle(5, 5, 2, 2)
-map.add_rectangle_puddle(6, 6, 2, 2)
-# map.add_rectangle_puddle(3, 3, 5, 5)
-#map.add_rectangle_puddle(0,0,10,10)
-epsilon = 5e1
-gamma = 0.97
+dim = [40,40]
+goal = [10,10]
+p = 0.5
 
-rng = np.random.default_rng()
+env = GridWorld(dim, goal, p)
 
-## Initialize
-seed = [250, 250, 9000, 9000]#np.round(rng.uniform(0,9999,4))
+while(1):
+    env.step(2)
+    env.render()
 
-env = AmbiguousPuddleWorld(map.get_coarsened_world(world_size,world_size), R, puddle_transition, list(seed))
-
-opt = Bellman(env)
-dst_opt = PignisticBellman(env)
-
-env.render("/home/jared/pomdp_ws/src/ambiguity-value-iteration/data/avi/fig")
+#env.render("/home/jared/pomdp_ws/src/ambiguity-value-iteration/data/avi/fig")
 
 # print("standard")
 # vi = VI(opt, epsilon, gamma)
