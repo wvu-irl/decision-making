938bba6ff9213790a28beb84e40a504a21886c48
Jared Beard <jbeard6@mix.wvu.edu>
2022-09-11 16:12:43 -0400

    updated mcgs_test

--- python/test/mcgs_test.py
+++ python/test/mcgs_test.py
@@ -8,6 +8,7 @@ import numpy as np
 import random
 
 from gym_envs.gridworld import GridWorld
+from gym_envs.gridtrap import GridTrap
 from gym_envs.sailing import Sailing
 from solvers.mcgs import MCGS
 from select_action.actions import *
@@ -18,17 +19,29 @@ alpha = 0
 dim = [25,25]
 goal = [5,5]
 p = 0
-sailing_test = False
-if not sailing_test:
+test_type = 1
+if test_type == 0:
+    #Env
+    dim = [40,40]
+    goal = [10,10]
     env = GridWorld(dim, goal, p)
     bounds = [0,1]
+elif test_type == 1:
+    dim = [35,10]
+    goal = [10,5]
+    D = 10
+    env = GridTrap(dim, goal, p)
+    bounds = [0,1]
 else:
+    #Env
+    dim = [50,50]
+    goal = [12,12]
     env = Sailing(dim, goal, p)
-    bounds = [-1, 5000]
+    bounds = [-401.11, 1100.99]
 
 import numpy as np
 #env2 = GridWorld(dim, goal, p)
-timeout = 10
+timeout = 2
 
 #Solver
 act_select_bounds = action_selection(mcgs_dm)
