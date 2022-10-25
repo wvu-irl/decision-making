da00d5c897257fbafdcb10933882257c6b7662e1
Jared Beard <jbeard6@mix.wvu.edu>
2022-10-25 03:18:52 -0400

    started multithreading

--- decision_making/analysis/multithread_collect.py
+++ decision_making/analysis/multithread_collect.py
@@ -8,126 +8,127 @@ sys.path.append(parent)
 
 from multiprocessing import Pool, Lock
 
+import itertools
 import json 
+import gym
+import custom_gym
 
-from planners import *
-from envs import *
+from planners.utils import *
+
+# Unrelated note: Sample MCTS graph like RRT. Then try to move twoards that subgoal, instead of normal sim. (similar to planning trees IROS paper)
+
 
 """_summary_ This file is intended to run a user specified
         decision making algorithm from those available in config
         
     
 """
-
-## CONFIG -------------------------------------------------
-algo = sys.argv[1]
-alg_config_file = sys.argv[2]
-env_config_file = sys.argv[3]
-if len(sys.argv) >= 5:
-    num_cores = sys.argv[4]
-else:
-    num_cores = 1
-if len(sys.argv) >= 6:
-    fp = sys.argv[5]
-else:
-    fp = None
-
-f = open(current + "../config/algorithms/" + alg_config_file +  ".json")
-alg_config = json.load(f)
-f = open(current + "../config/envs/" + env_config_file +  ".json")
-env_config = json.load(f)   
-
-# SETUP CONFIG -------------------
-
-# ENVS
-if env_config == "grid world":
-    pass
-elif env_config == "sailing":
-    pass
-elif env_config == "grid trap":
-    pass
-
-
-# ALGS
-if env_config == "grid world":
-    pass
-elif env_config == "sailing":
-    pass
-elif env_config == "grid trap":
-    pass
-
-# PRINT CONFIG -------------------
-    
-print("Testing ", algo, " in ", env_name)
-print("Rendering? ", do_render)
-if fp != None:
-    print("Saving output to ", fp)
-
-
+   
 
 ## Evaluation -------------------------------------------------
-def runWrapper(obj): 
-    # Run each trial
-    for t in range(num_time_steps):
-        # Display map for current time step, if only one trial
-        if config.enable_plots and num_threads == 1:
-            displayMap(obj, plt, map_fig, map_ax)
-            if save_plots == 1:
-                map_fig.savefig("figures/fig%d.png" % t)
-            print("\nt = {0}".format(t))
+def runWrapper(params : dict): 
+    env = gym.make(params["env"]["env"],max_episode_steps = params["mt"]["max_timesteps"], _params=params["env"]["params"])
+    planner = get_agent(params["alg"],params["env"])
 
-        if save_prev_exp:
-            lock.acquire()
-            prev_exp_data.record(obj, t)
-            lock.release()
+    # Simulate
+    print("-----------")
+    s = env.reset()
+    done = False
+    ts = 0
+    accum_reward = 0
+    while(not done):
+        a = planner.search(s, alg_config["search"])
+        s, r,done,info = env.step(a)
+        ts += 1
+        accum_reward += r
 
-        terminal_condition = obj.simulationStep(t)
-        if slow_mode and num_threads == 1:
-            time.sleep(0.5)
+    return [ts, accum_reward]
+
+        # if save_prev_exp:
+        #     lock.acquire()
+        #     prev_exp_data.record(obj, t)
+        #     lock.release()
+
+        # terminal_condition = obj.simulationStep(t)
+        # if slow_mode and num_threads == 1:
+        #     time.sleep(0.5)
         
-        if ((t == num_time_steps - 1) or (enable_terminal_condition and terminal_condition)):
-            # Save final step of prev exp if at final time step
-            if save_prev_exp:
-                lock.acquire()
-                prev_exp_data.record(obj, t)
-                lock.release()
+        # if ((t == num_time_steps - 1) or (enable_terminal_condition and terminal_condition)):
+        #     # Save final step of prev exp if at final time step
+        #     if save_prev_exp:
+        #         lock.acquire()
+        #         prev_exp_data.record(obj, t)
+        #         lock.release()
 
-            # Display final map if at final time step
-            if config.enable_plots and num_threads == 1:
-                displayMap(obj, plt, map_fig, map_ax)
-                if save_plots == 1:
-                    t = t+1
-                    map_fig.savefig("figures/fig%d.png" % t)
+        #     # Display final map if at final time step
+        #     if config.enable_plots and num_threads == 1:
+        #         displayMap(obj, plt, map_fig, map_ax)
+        #         if save_plots == 1:
+        #             t = t+1
+        #             map_fig.savefig("figures/fig%d.png" % t)
 
         # End simulation early if terminal condition reached
-        if enable_terminal_condition and terminal_condition:
-            break
+        # if enable_terminal_condition and terminal_condition:
+        #     break
+    
+def poolHandler(alg_config, env_config, mt_config):
 
-    return obj
-    
-def poolHandler():
-    # Initialize worlds
-    print("Initializing worlds...")
-    sim_worlds = [World(i, food_layer, home_layer, obstacle_layer, robot_layer, robot_personality_list, perception_range, battery_size, heading_size, policy_filepath_list, v_filepath_list, q_filepath_list, arbitration_type_list, use_prev_exp, prev_exp_filepath, num_time_steps, heading_change_times, food_respawn, real_world_exp=False, manual_control=use_manual_control) for i in range(num_monte_carlo_trials)]
-    
+    temp = []
+    temp.append(mt_config["epsilon"])
+    temp.append(mt_config["delta"])
+    temp.append(mt_config["horizon"])
+    temp.append(mt_config["alpha"])
+    temp.append(mt_config["initial_state"]) # These should be provided as a bumber and sampled
+    temp.append(mt_config["goal_state"])    # These should be prior sampled
+    temp.append(mt_config["world_size"])
+    temp.append(mt_config["probabilities"])
+
+    temp = list(itertools.product(*temp)) 
+
+
+    trials = []
+    for i in range(mt_config["n_trials"]):
+        for el in temp:
+            trials.append(el)()
+
+    fp = "" + mt_config["file"] + ".npy"
+    # Save this as a csv file.  or np file
+
     # Run pool of Monte Carlo trials
     print("Beginning Monte Carlo trials...")
-    if num_threads > 1:
+    if mt_config["N_threads"] > 1:
         # Initialize multiprocessing pool and map objects' run methods to begin simulation
-        p = Pool(num_threads)
+        p = Pool(mt_config["N_threads"])
         sim_worlds = p.map(runWrapper, sim_worlds)
     else:
         # Run each trial sequentially, for debugging purposes, with no multiprocessing
-        for i in range(num_monte_carlo_trials):
-            runWrapper(sim_worlds[i])
+        for t in trials:
+            #update alg config
+            #update env_config
+            runWrapper(t)
     print("Simulations complete.")
 
     # Save results
+    
     saveResultsFile(results_filename, sim_worlds)
     if save_prev_exp:
         prev_exp_data.save(prev_exp_filepath)
 
+
+
 if __name__=='__main__':
-    poolHandler()
+    ## CONFIG -------------------------------------------------
+    alg_config_file = sys.argv[1]
+    env_config_file = sys.argv[2]
+    mt_config_file = sys.argv[3]
+
+    f = open(current + "/../config/algorithms/" + alg_config_file +  ".json")
+    alg_config = json.load(f)
+    f = open(current + "/../config/envs/" + env_config_file +  ".json")
+    env_config = json.load(f)  
+    f = open(current + "/../config/multithread/" + mt_config_file +  ".json")
+    mt_config = json.load(f)    
+
+    poolHandler(alg_config, env_config, mt_config)
 
 ## Post Processing---------------------------------------------
