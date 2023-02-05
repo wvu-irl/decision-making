d29e000e8194b581c368df9f2bbd69951b8f6bd9
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-02-05 15:23:24 -0500

    started purge

--- decision_making/analysis/purge_data.py
+++
@@ -1,5 +0,0 @@
-# for each element.
-
-#command line pkl file name argname1 val1 an2 v2 .....
-#iterate through and delete all
-# or if receives "all", delete everything
--- decision_making/utils.py
+++
@@ -1,4 +0,0 @@
-
-
-def get_distance(s1, s2):
-        return ((s1[0]-s2[0])**2 + (s1[1]-s2[1])**2)**0.5
