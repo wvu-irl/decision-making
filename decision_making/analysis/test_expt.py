0d47fa20cdafe110b41027ce4d395c14fb7204c8
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-02-07 19:13:13 -0500

    updated test expt

--- decision_making/analysis/test_expt.py
+++ decision_making/analysis/test_expt.py
@@ -3,8 +3,10 @@ import pandas as pd
 def test(params: dict):
     if "a" in params["values"]:
         print("A ", params["values"]["a"])
+        val = {"name": "a", "value": params["values"]["a"]}
     if "b" in params["values"]:
         print("B ", params["values"]["b"])
+        val = {"pseudonym": "b", "value": params["values"]["b"]}
+    return pd.DataFrame([val])
 
     ## Save the param to a pandas series and return it
-    return pd.Series(params["values"]) 
