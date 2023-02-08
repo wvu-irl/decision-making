4d8c18c484e9b335e89ca2a80223ceac33842f00
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-02-07 22:06:20 -0500

    compatibility updates for test expt

--- decision_making/analysis/test_expt.py
+++ decision_making/analysis/test_expt.py
@@ -1,6 +1,11 @@
 import pandas as pd 
+import time
 
 def test(params: dict):
+    my_str = ""
+    for i in range(100000):
+        my_str += "a"
+        # print(my_str)
     if "a" in params["values"]:
         print("A ", params["values"]["a"])
         val = {"name": "a", "value": params["values"]["a"]}
