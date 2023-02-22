9fbe17f3a48a217e4f7a7b42d588fdc3a8b47043
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-02-21 21:29:07 -0500

    irl_gym not found fix

--- decision_making/analysis/rl_expt.py
+++ decision_making/analysis/rl_expt.py
@@ -16,7 +16,7 @@ import irl_gym
 import gymnasium as gym
 
 def rl_expt(params : dict):
-    
+
     """
     Simulates and saves a single experimental trial
     
@@ -27,28 +27,28 @@ def rl_expt(params : dict):
     params["envs"]["state"] = deepcopy(s)
     planner = get_agent(params["algs"],params["envs"])
 
-    # done = False
-    # ts = 0
-    # accum_reward = 0
-    # while(not done):
-    #     a = planner.evaluate(s, params["algs"]["search"])
-    #     s, r,done, is_trunc, info = env.step(a)
-    #     done = done or is_trunc
-    #     ts += 1
-    #     accum_reward += r
-    #     if params["envs"]["params"]["render"] != "none":
-    #         env.render()
+    done = False
+    ts = 0
+    accum_reward = 0
+    while(not done):
+        a = planner.evaluate(s, params["algs"]["search"])
+        s, r,done, is_trunc, info = env.step(a)
+        done = done or is_trunc
+        ts += 1
+        accum_reward += r
+        if params["envs"]["params"]["render"] != "none":
+            env.render()
     
-    # if ts < params["envs"]["max_time"]:
-    #     accum_reward += (params["envs"]["max_time"]-ts)*r
+    if ts < params["envs"]["max_time"]:
+        accum_reward += (params["envs"]["max_time"]-ts)*r
     
-    # data_point = nd.unstructure(params)
-    # data_point["time"] = ts
-    # data_point["r"] = accum_reward
-    # if "pose" in data_point and "goal" in data_point:
-    #     data_point["distance"] = np.linalg.norm(np.asarray(data_point["pose"][1:2])-np.asarray(data_point["goal"]))
-    # data_point["final"] = deepcopy(s)
-    # if "pose" in s and "goal" in data_point:
-    #     data_point["final_distance"] = np.linalg.norm(np.asarray(s["pose"][1:2])-np.asarray(data_point["goal"]))
+    data_point = nd.unstructure(params)
+    data_point["time"] = ts
+    data_point["r"] = accum_reward
+    if "pose" in data_point and "goal" in data_point:
+        data_point["distance"] = np.linalg.norm(np.asarray(data_point["pose"][1:2])-np.asarray(data_point["goal"]))
+    data_point["final"] = deepcopy(s)
+    if "pose" in s and "goal" in data_point:
+        data_point["final_distance"] = np.linalg.norm(np.asarray(s["pose"][1:2])-np.asarray(data_point["goal"]))
             
-    return pd.DataFrame([{"a":1,"b":2}])
+    return pd.DataFrame([data_point])
